09fb6838bf987955ef16f12e1bb2f2ce829f68a4
Amanda Nogueira Meneghin <amanda.meneghin@hadrion.com.br>
2023-02-03 16:47:53 -0300

    ajuste

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('server/db.json');
 const middlewares = jsonServer.defaults();
 const db = require('./db.json');
 const fs = require('fs');
@@ -79,20 +78,6 @@ server.get('/itens', (req, res) => {
   res.jsonp(newData)
 })
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-// server.use(jsonServer.bodyParser)
-
-// server.use((req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now()
-//   }
-//   // Continue to JSON Server router
-//   next()
-// })
-
-// Use default router
-// server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
